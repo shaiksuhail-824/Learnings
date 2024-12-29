15d0eb663f09429bafc7808347be18ff0bafcd6b
Your Name <youremail@example.com~>
2024-12-29 23:14:38 +0530

    suhail

---
+++ assignment operator.py
@@ -0,0 +1,21 @@
+a=30
+ #a+=5 # a=a+5
+#print("a",a)
+#a-=5 # a=a-5
+#print("a-=5",a)
+#a*=5 # a=a*5
+#print("a*=5",a)
+#a/=5 # a=a/5
+#print("a/=5",a)
+#a//=5 # a=a//5
+#print("a//=5",a)
+#a&=5 # a=a&5
+#print("a&=5",a)
+#a%=4 # a=a%4
+#print("a%=4",a)
+a**=3 # a=a**3
+print("a**=3",a)
+
+
+
+
